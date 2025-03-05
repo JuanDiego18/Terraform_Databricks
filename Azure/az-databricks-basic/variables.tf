abee3a4da672d96853dff10b16ebea3a20a595e4
JuanDiego18 <jdiegov2324@gmail.com>
2025-03-05 12:50:15 -0600

    here we go again

--- Azure/az-databricks-basic/variables.tf
+++ Azure/az-databricks-basic/variables.tf
@@ -39,6 +39,7 @@ variable "dbx_client_secret" {
 variable "create_resource_group" {
   type = bool
 }
+
 variable "spoke_resource_group_name" {
   type        = string
   description = "(Required) The name of the Resource Group to create"
